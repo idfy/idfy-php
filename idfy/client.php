72d7dac949ae4387b7f3a40b5054c20fc7fcf253
Sagar Khatri <sagar.khatri@idfy.com>
2018-09-14 15:11:48 +0530

    First release

---
+++ idfy/client.php
@@ -0,0 +1,204 @@
+<?php
+// include '../errors.php';
+// include '../constants/tasks.php';
+
+set_include_path(realpath(dirname(__FILE__)));
+// require(dirname(__FILE__) . '/errors.php');
+// require(dirname(__FILE__) . '/constants/tasks.php');
+require(dirname(__FILE__) . '/constants/url.php');
+require(dirname(__FILE__) . '/utility/utility.php');
+
+class Client extends Tasks{
+
+    var $DEFAULTS;
+    var $auth;
+    var $base_url;
+    var $headers;
+
+    function __construct($autho,array $options){
+
+        $this -> DEFAULTS['base_url_v2'] = Url::$BASE_URL_V2;
+        $this -> DEFAULTS['base_url_v3'] = Url::$BASE_URL_V3;
+        $this -> DEFAULTS['headers'][0] = 'Content-Type: application/json';
+        array_push($this->DEFAULTS['headers'],"apikey: ".($autho));
+        $this -> auth = $autho;
+        $this -> base_url = $this -> set_base_url($options);
+        
+        if ($options){
+            $this -> headers = $this -> validate_headers($options);
+        }else{
+            $this-> headers = $this->DEFAULTS['headers'];
+        }
+        
+        
+    }
+
+    function set_base_url(array $options){
+        /*
+        Set the api_endpoint based on client's initiation
+        :param options: dict obj
+        :return: api_endpoint url
+        */
+        $api_endpoint = $this -> DEFAULTS['base_url_v2'];       
+        
+        if (array_key_exists('url', $options)){
+            if (!is_string($options['url'])){
+                throw new BadRequestError('URL is not a string');
+            }
+            else{
+                $api_endpoint = $options['url'];
+            }
+        }
+        return $api_endpoint;
+    }
+
+    public function validate_headers($option){
+    /*
+        Validate headers and update headers if provided during client initiation
+        :param options: kwargs
+        :return: headers
+    */
+        if (array_key_exists('headers', $option)){
+            if (is_array($option['headers'])){
+                if (!array_key_exists('apikey',$option['headers'])){
+                    
+                    throw new BadRequestError("No API-key provided. client_1 = Client('<YOUR_API_KEY>')");
+                }
+                //Update self.headers
+                foreach ($option['headers'] as $key=>$value)
+                {   
+                   
+                    array_push($this -> DEFAULTS['headers'],$key.": ". $value);
+  
+                }
+                // 
+            }
+            else{
+                throw new BadRequestError('Invalid headers provided. 
+                Correct format: array ("content-Type"=> "application/json")');
+            }
+        }
+        return $this -> DEFAULTS['headers'];
+    }
+
+    public function post_request(string $task_type,string $task_id, array $data, $group_id=NULL){
+    /*
+        post_request to EVE-API
+        :param task_type: task_type
+        :param task_id: task_id
+        :param data: part of the request_body
+        :param group_id: group_id
+        :return: API_request's request_id and status
+    */
+        $req_obj = new Utility($task_type, $task_id, $data, $group_id=NULL);
+        $req_body = $req_obj->validate_request_arguments($api_version="v2");
+        $url = $this->DEFAULTS['base_url_v2'];
+        
+        //Making POST request
+        $curl = curl_init();
+
+        curl_setopt_array($curl, array(
+        CURLOPT_URL => $this->base_url,
+        CURLOPT_RETURNTRANSFER => true,
+        CURLOPT_ENCODING => "",
+        CURLOPT_MAXREDIRS => 10,
+        CURLOPT_TIMEOUT => 30,
+        CURLOPT_HTTP_VERSION => CURL_HTTP_VERSION_1_1,
+        CURLOPT_CUSTOMREQUEST => "POST",
+        CURLOPT_POSTFIELDS => $req_body,
+        CURLOPT_HTTPHEADER => $this->headers
+        ));
+        
+        $response = curl_exec($curl);
+        $err = curl_error($curl);
+
+        curl_close($curl);
+
+        if ($err) {
+        echo "cURL Error #:" . $err;
+        } else {
+        echo $response;
+        }
+        
+        return $response;
+    
+    }
+
+    public function get_request($request_id=NULL, $group_id=NULL, $task_id=NULL){
+    /*
+        get request to retrieve response from EVE-API with request_id
+        :param request_id: request_id
+        :param group_id: group_id
+        :param task_id: task_id
+        :return: API_call's response
+    */
+        $params = array();
+        if (!$request_id && !$group_id && $task_id){
+            throw new BadRequestError("Invalid request. Provide atleast one of request_id, group_id or task_id.");
+        } 
+        if (!is_string($request_id) && $request_id){
+            throw new BadRequestError("Invalid request_id format. Expected format is string.");
+        }
+        if ($request_id){
+            $params['request_id'] = $request_id;
+        }
+        if (!is_string($group_id) && $group_id){
+            throw new BadRequestError("Invalid group_id format. Expected format is string.");
+        }
+        if ($group_id){
+            $params['group_id'] = $group_id;
+        }
+        if (!is_string($task_id) && $task_id){
+            throw new BadRequestError("Invalid task_id format. Expected format is string.");
+        }
+        if ($task_id){
+            $params['task_id'] = $task_id;
+        }
+
+        $dat = '';
+        foreach($params as $key=>$val){
+            $dat.=$key.'='.$val.'&';
+        }
+        $dat = trim($dat,'&');
+        //Sending GET Request:
+        $url = $this->DEFAULTS['base_url_v2'];
+        $url = $url."?".$dat;
+        print_r ($url);
+        
+        $curl = curl_init();
+
+        curl_setopt_array($curl, array(
+        CURLOPT_URL => $url,
+        CURLOPT_RETURNTRANSFER => true,
+        CURLOPT_ENCODING => "",
+        CURLOPT_MAXREDIRS => 10,
+        CURLOPT_TIMEOUT => 30,
+        CURLOPT_HTTP_VERSION => CURL_HTTP_VERSION_1_1,
+        CURLOPT_CUSTOMREQUEST => "GET",
+        CURLOPT_HTTPHEADER => $this->headers
+        ));
+
+        $response = curl_exec($curl);
+        $err = curl_error($curl);
+
+        curl_close($curl);
+
+        if ($err) {
+        echo "cURL Error #:" . $err;
+        } else {
+        echo $response;
+        }
+        return $response;
+    }
+    
+    
+}
+$arr = array("url"=>"google.com");
+$a = new Client("77484e44-db92-4a64-9584-0cc1798cd44e",array("apikey"=> "77484e44-db92-4a64-9584-0cc1798cd44e","Content-Type"=>"application/json"));
+
+
+// print_r ($a -> post_request("pan_validation","php-sdk1",array("doc_url"=>"https://s3-ap-southeast-1.amazonaws.com/addressify-demo/eve+api+images/PAN+LATEST/IMG_20171218_161405308.jpg")));
+
+print_r ($a->get_request($request_id=NULL,$group_id="the-long-bright-dark"));
+
+
---
+++ idfy/utility/utility.php
@@ -0,0 +1,157 @@
+<?php
+/*
+Request object initiation and validation
+*/
+set_include_path(realpath(dirname(__FILE__)));
+require(dirname(__FILE__) . '/../errors.php');
+require(dirname(__FILE__) . '/../constants/tasks.php');
+
+class Utility extends Tasks{
+/*
+    API request-object initiation and validation module
+*/
+    var $DEFAULTS;
+    var $task_id;
+    var $task_type;
+    var $data;
+    var $group_id;
+
+    function __construct(string $task__type,string $task__id, array $data_, $group__id=NULL){
+        
+        $tasks = Tasks::$_tasks_config;
+        // print_r($tasks);
+        $this -> DEFAULTS['tasks_config'] = $tasks;
+        $this -> task_id = $task__id;
+        $this -> task_type = $task__type;
+        $this -> data = $data_;
+        $this -> group_id = $group__id;
+    }
+
+    function update_api_body(){
+        /*
+        Update the api_body 
+        :return: updated req_body
+        */
+        $req_body = array(
+                        'tasks'=>array(array(
+                                    'type'=> $this->task_type,
+                                    'task_id'=> $this->task_id,
+                                    'data'=> $this->data)));
+        
+        if ($this->group_id){
+            $req_body['tasks'][0]['group_id'] = $this->group_id;
+        }
+
+        return json_encode($req_body);
+    }
+
+    function validate_request_data(string $api_version){
+        /*
+        validate request_data. Checks presence of mandate_fields
+        :param data: part of request_body
+        :param task_type: task_type
+        :return: None
+        */
+
+        $all_task_data_fields = array();
+        $input_data_keys = array_keys($this->data);
+
+        $task_schema = $this->DEFAULTS['tasks_config'][$api_version]['data_schema'][$this->task_type];
+        $mandate_task_data_fields = $task_schema['mandate_fields'];
+
+        if ($mandate_task_data_fields){
+            $all_task_data_fields = array_merge($all_task_data_fields,$mandate_task_data_fields);
+
+            foreach ($mandate_task_data_fields as $key){
+                if (!in_array($key, $input_data_keys)){
+                    throw new BadRequestError("Insufficient data fields provided \n". implode("\t",$mandate_task_data_fields));
+                }
+            }
+
+        }
+        $optional_task_data_fields = $task_schema['optional_fields'];
+
+        if ($optional_task_data_fields){
+            $all_task_data_fields = array_merge($all_task_data_fields,$optional_task_data_fields);
+        }
+
+        $any_task_data_fields = $task_schema['any'];
+        if ($any_task_data_fields){
+            $all_task_data_fields = array_merge($all_task_data_fields,$any_task_data_fields);
+
+            $any_fields_flag = FALSE;
+
+            foreach ($any_task_data_fields as $key){
+                if (array_key_exists($key, $input_data_keys)){
+                    $any_fields_flag = TRUE;
+                    break;
+                }
+            }
+
+            if (!$any_fields_flag){
+                throw new BadRequestError("Insufficient data fields provided. Expected any of these fields \n".implode(" ",$any_task_data_fields));
+            }
+
+        }
+
+        //Checking if any extra fields provided which are not required
+        foreach ($input_data_keys as $key){
+            if (!in_array($key, $all_task_data_fields)){
+                throw new BadRequestError("Unexpected data field provided in data ".$key." in ".implode(" ",$this->data)."\n"."Acceptable fields - ".implode(" ",$all_task_data_fields));
+            }
+        }
+
+    }
+
+    function validate_request_arguments( $api_version){
+        /*
+        Validate json_body part of the api_request
+        :param task_type: task_type
+        :param task_id: task_id
+        :param data: part of request_body
+        :param api_version: v2/v3
+        :param group_id: group_id
+        :return: req_body
+        */
+        // print_r ($this->data);
+        // print_r ($this->DEFAULTS['tasks_config'][$api_version]['data_schema']);
+        // print_r ($this-> task_type);
+        if (($this->check_dict($this -> data))){
+            throw new BadRequestError("Invalid data format. Expected format is associative array, eg: array('doc_url'=> '<URL>') or ['doc_url' => '<URL>']");
+        }
+        if (!is_string($this->task_id)){
+            throw new BadRequestError("Invalid task_id format. Expected format is string, eg: '<TASK_ID>'");
+        }
+        if (!is_string($this->task_type)){
+            throw new BadRequestError("Invalid task_id format. Expected format is string, eg: '<TASK_ID>'");
+        }elseif (!array_key_exists($this->task_type,$this->DEFAULTS['tasks_config'][$api_version]['data_schema'])){
+            throw new BadRequestError("Invalid task_type requested. Refer the doc for task_types - ".
+            "https://api-docs.idfy.com/v2/#task-types");
+        }
+        if (!is_string($this->task_id)){
+            throw new BadRequestError("Invalid task_id format. Expected format is string, eg: '<TASK_ID>'");
+        }
+        if (isset($this->group_id)){
+            if (!is_string($this->group_id)){
+                throw new BadRequestError("Invalid group_id format. Expected format is string, eg: '<GROUP_ID>'");
+            }
+        }
+        $this -> validate_request_data($api_version);
+        $req_body = $this->update_api_body();
+
+        return $req_body;
+    }
+
+    //Function to check if given input is an associative array (i.e., A dict)
+    function check_dict($input){
+        if (!is_array($input)){
+            return FALSE;
+        }
+
+        return count(array_unique(array_map("is_string", array_keys($input)))) >= 2;
+
+    }
+
+    
+}
+
